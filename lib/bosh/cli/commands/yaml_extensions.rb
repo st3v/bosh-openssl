3a3b77a8e39f75ac734869f7b86b175cbca76e7c
David Laing <david@davidlaing.com>
2016-05-06 16:11:14 +0100

    Add extension methods to BOSH's YAML rendering

---
+++ lib/bosh/cli/commands/yaml_extensions.rb
@@ -0,0 +1,28 @@
+require "bosh/openssl"
+
+module Bosh::Cli::Command
+  class YamlExtensions < Base
+
+    ::BoshExtensions.module_eval do
+      def get_password name
+        include ::Bosh::Openssl::Helpers
+        password name
+      end
+      def get_certificate cert_name, key_name
+        include ::Bosh::Openssl::Helpers
+        out = certificate(cert_name, key_name).to_s
+        out.dump
+      end
+      def get_public_key name
+        include ::Bosh::Openssl::Helpers
+        out = public_key(name).to_s
+        out.dump
+      end
+      def get_private_key name
+        include ::Bosh::Openssl::Helpers
+        out = private_key(name).to_s
+        out.dump.to_s
+      end
+    end
+  end
+end
